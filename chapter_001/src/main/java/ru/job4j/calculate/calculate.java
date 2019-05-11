60248effa907a6fb56c17e6d6c0ed06c211ac76e
vadimkasochi <inetitv@gmail.com>
2019-05-11 21:42:44 +0300

    е приводится список требований к оформлению кода.

--- chapter_001/src/main/java/ru/job4j/calculate/calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/calculate.java
@@ -1,10 +1,20 @@
+/**
+* Package for calculate task.
+*
+* @author Vadim Rylkov (inetitv@gmail.com)
+* @version 1
+* @since 0.1
+*/
 package chapter_001.src.main.java.ru.job4j.calculate;
 
 public class calculate {
 	public String echo(String value) {
 		return String.format("%s %s %s" , value, value, value);
 	}
-	
+	/**
+* main \
+* @param args - args/
+*/
 	public static void main(String[] args) {
 		calculate calc = new calculate();
 		System.out.println(calc.echo("afaf"));
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vadim Rylkov (inetitv@gmail.com)
+* @version 1
+* @since 0.1
+*/
+package ru.job4j.calculate;
