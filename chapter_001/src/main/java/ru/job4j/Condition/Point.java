3639c098eabce41115b7aa22dddc59dff90caaf3
vadimkasochi <inetitv@gmail.com>
2019-05-15 23:04:34 +0300

    4.3. Вычисление площади треугольника[#141280] Версия с ошибкой

--- chapter_001/src/main/java/ru/job4j/Condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/Condition/Point.java
@@ -2,6 +2,7 @@ package ru.job4j.Condition;
 
 public class Point {
     public double distance(int x1, int y1, int x2, int y2) {
+
         double first = Math.pow(x2 - x1, 2);
         double neFirst = Math.pow(y2 - y1, 2);
         return Math.sqrt(first) + Math.sqrt(neFirst);
---
+++ chapter_001/src/main/java/ru/job4j/Triangle/Triangle.java
@@ -0,0 +1,51 @@
+package ru.job4j.Triangle;
+
+
+import ru.job4j.Condition.Point;
+
+public class Triangle {
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     * Формула.
+     * (a + b + c) / 2
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return Перимент.
+     */
+    public double period(double a, double b, double c) {
+            return (a+b+c)/2;
+    }
+    /**
+     * Метод должен вычислить прощадь треугольканива.
+     * Формула.
+     * √ p *(p - a) * (p - b) * (p - c)
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double rsl = -1;
+        double a = new Point().distance(x1, y1, x2, y2);
+        double b = new Point().distance(x2, y2, x3, y3);
+        double c = new Point().distance(x1, y1, x3, y3);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            double x =  p * (p - a) * (p - b) * (p - c);
+            rsl = Math.sqrt(x);
+        }
+        return rsl;
+    }
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     * @param a Длина от точки a b.
+     * @param b Длина от точки a c.
+     * @param c Длина от точки b c.
+     * @return  true or false.
+     */
+    private boolean exist(double a, double c, double b) {
+
+        return a > 0 & b > 0 & c > 0 ? true : false;
+    }
+}
