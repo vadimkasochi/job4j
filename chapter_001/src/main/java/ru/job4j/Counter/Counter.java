72c976d68790f1adbb8e814746b0e56728a75dc7
vadimkasochi <inetitv@gmail.com>
2019-05-17 00:10:42 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#141285]

---
+++ chapter_001/src/main/java/ru/job4j/Counter/Counter.java
@@ -0,0 +1,14 @@
+package ru.job4j.Counter;
+
+    public class Counter {
+        public int add(int start, int finish) {
+            int sum = 0;
+            for (int i = 0; 10 >= i; i++){
+                if (i % 2 == 0){
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+    }
+
---
+++ chapter_001/src/test/java/ru/job4j/CounterTest/CounterTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.CounterTest;
+
+import org.junit.Test;
+import ru.job4j.Counter.Counter;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+        @Test
+        public void sumOfPositiveNumber() {
+            Counter counter = new Counter();
+            int result = counter.add(1, 2);
+            assertThat(result, is(30));
+        }
+    }
+
