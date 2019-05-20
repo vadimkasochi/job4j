0dd1607dd5a26a1490b154e8d4dbcef73b56e018
vadimkasochi <inetitv@gmail.com>
2019-05-20 23:49:09 +0300

    5.3. Построить шахматную доску в псевдографике.[#141289]

---
+++ chapter_001/src/test/java/ru/job4j/PaintTest/PaintTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.PaintTest;
+
+import org.junit.Test;
+import ru.job4j.Paint.Paint;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class PaintTest {
+
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+    @Test
+    public void whenPyramid4mid() {
+        Paint paint = new Paint();
+        String rst = paint.Trl(5);
+        System.out.println(rst);
+        assertThat(rst, is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+    @Test
+    public void whenPyramid5mid() {
+        Paint paint = new Paint();
+        String rst = paint.Trl(5);
+        System.out.println(rst);
+        assertThat(rst, is(
+                new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                        .add("    ^    ")
+                        .add("   ^^^   ")
+                        .add("  ^^^^^  ")
+                        .add(" ^^^^^^^ ")
+                        .add("^^^^^^^^^")
+                        .toString()
+                )
+        );
+    }
+}
